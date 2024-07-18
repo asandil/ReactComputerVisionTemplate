70d50cefd55e49e272a657d8fb70095818d3fe45
Abhijeet Sandil <abhijeetsandil@gmail.com>
2024-07-18 15:36:44 +0530

    deployment fix

--- src/App.js
+++ src/App.js
@@ -7,26 +7,25 @@ import "./App.css";
 //import { drawRect } from "./utilities"; // Assuming you have a drawRect utility
 
 function App() {
+	const drawRect = (detections, ctx) => {
+		detections.forEach((prediction) => {
+			// Get prediction results
+			const [x, y, width, height] = prediction["bbox"];
+			const text = prediction["class"];
 
-	export const drawRect = (detections, ctx) => {
-	detections.forEach((prediction) => {
-		// Get prediction results
-		const [x, y, width, height] = prediction["bbox"];
-		const text = prediction["class"];
+			// Set styling
+			ctx.strokeStyle = "green";
+			ctx.font = "18px Arial";
 
-		// Set styling
-		ctx.strokeStyle = "green";
-		ctx.font = "18px Arial";
+			// Draw rectangles and text
+			ctx.beginPath();
+			ctx.fillStyle = "green";
+			ctx.fillText(text, x, y);
+			ctx.rect(x, y, width, height);
+			ctx.stroke();
+		});
+	};
 
-		// Draw rectangles and text
-		ctx.beginPath();
-		ctx.fillStyle = "green";
-		ctx.fillText(text, x, y);
-		ctx.rect(x, y, width, height);
-		ctx.stroke();
-	});
-};
-	
 	const webcamRef = useRef(null);
 	const canvasRef = useRef(null);
 
