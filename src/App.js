45ca129d4d75821b6113b2be95eccda26696014a
Abhijeet Sandil <abhijeetsandil@gmail.com>
2024-07-18 15:33:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,29 @@ import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import "./App.css";
-import { drawRect } from "./utilities"; // Assuming you have a drawRect utility
+//import { drawRect } from "./utilities"; // Assuming you have a drawRect utility
 
 function App() {
+
+	export const drawRect = (detections, ctx) => {
+	detections.forEach((prediction) => {
+		// Get prediction results
+		const [x, y, width, height] = prediction["bbox"];
+		const text = prediction["class"];
+
+		// Set styling
+		ctx.strokeStyle = "green";
+		ctx.font = "18px Arial";
+
+		// Draw rectangles and text
+		ctx.beginPath();
+		ctx.fillStyle = "green";
+		ctx.fillText(text, x, y);
+		ctx.rect(x, y, width, height);
+		ctx.stroke();
+	});
+};
+	
 	const webcamRef = useRef(null);
 	const canvasRef = useRef(null);
 
